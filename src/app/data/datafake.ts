f2e266937baf5d15d6fab9b4c86186852f9c7ae4
junotdutra <junot.rj@gmail.com>
2023-10-05 14:58:50 -0400

    Conteúdos dinâmicos

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "qweqwe",
+    "description": "descrição",
+    "photoCover": "https://lumiere-a.akamaihd.net/v1/images/iron_man_marvel_d9ce0209.jpeg?region=36,0,713,399",
+  },
+  {
+    "id": "2",
+    "title": "qweqwe",
+    "description": "descrição",
+    "photoCover": "https://lumiere-a.akamaihd.net/v1/images/iron_man_marvel_d9ce0209.jpeg?region=36,0,713,399",
+  },
+  {
+    "id": "3",
+    "title": "qweqwe",
+    "description": "descrição",
+    "photoCover": "https://boitempoeditorial.files.wordpress.com/2018/02/zizek-pantera-negra-blog.jpg",
+  },
+  {
+    "id": "4",
+    "title": "qweqwe",
+    "description": "descrição",
+    "photoCover": "https://s2-techtudo.glbimg.com/z-3568TCFtey_TPWtVRyWkfVfJw=/0x0:1280x720/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/4/C/eoA6AfQKeyRQGoO5q7bQ/xmen-texto.png",
+  }
+];
+
+
+
+
